3418298f3f0320da81cc692a3ad0abc41c8865c3
sandeep <sandeepkumar13975@email.com>
2023-04-10 13:59:12 +0530

    carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,76 +10,63 @@ const Carousel = () => {
       text: "We elevate the vision and the strategy",
       buttonText: "How It Works",
       shortText: "Elevate vision",
-      isActive: true,
     },
     {
       image: business02,
       text: "We turns ideas into business",
       buttonText: "Purchase Now",
       shortText: "Turn ideas into business",
-      isActive: false,
     },
     {
       image: business03,
       text: "Take your business to the next level",
       buttonText: "Try It Now",
       shortText: "Take your business to the next level",
-      isActive: false,
     },
   ];
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  const [activeIndex, setIndex] = useState(0);
-
-  //   useEffect(() => {
-  //     setTimeout(() => {
-  //       if (activeIndex + 1 === carouselDetails.length) {
-  //         setIndex(0);
-  //       } else {
-  //         setIndex(activeIndex + 1);
-  //       }
-  //     }, 5000);
-  //   }, [activeIndex]);
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      if (activeIndex === carouselDetails.length - 1) {
+        setActiveIndex(0);
+      } else {
+        setActiveIndex(activeIndex + 1);
+      }
+    }, 5000);
+    return () => {
+      clearInterval(timeout);
+    };
+  }, [activeIndex]);
 
   return (
     <div className="carousel__container">
-      <div className="carousel__backdrop"></div>
-      <div
-        className="carousel__image"
-        style={{
-          backgroundImage: `url(${carouselDetails[activeIndex]?.image})`,
-        }}
-      ></div>
-      <div className="carousel__body">
-        {carouselDetails.map((slide, slideIndex) => {
-          console.log(slideIndex, activeIndex);
-          return (
-            <div
-              className="active-slide"
-              style={{ display: activeIndex === slideIndex ? "block" : "none" }}
-              key={slideIndex}
-            >
-              <div className="carousel__content">
-                <h2 className="carousel__heading">{slide?.text}</h2>
-                <button className="carousel__button">
-                  {slide?.buttonText}
-                </button>
-              </div>
+      {carouselDetails.map((item, index) => {
+        return (
+          <div
+            className={`carousel__image`}
+            style={{
+              backgroundImage: `linear-gradient(to right, rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(${item.image})`,
+              transform: `translateX(${-1 * ((index - activeIndex) * 100)}%)`,
+            }}
+            key={index}
+          >
+            <div className="carousel__body">
+              <h2 className="carousel__heading">{item.text}</h2>
+              <button className="carousel__button">{item.buttonText}</button>
             </div>
-          );
-        })}
-      </div>
-
+          </div>
+        );
+      })}
       <div className="carousel__footer">
-        {carouselDetails?.map((item, index) => {
+        {carouselDetails.map((item, index) => {
           return (
             <div className="timeline" key={index}>
               <div className="bar-container">
                 <div className="bar"></div>
-                {index === activeIndex && <div className="active-bar"></div>}
+                {activeIndex === index && <div className="active-bar"></div>}
               </div>
-              <div className={`info ${item.isActive && "active"}`}>
-                {item.shortText}
-              </div>
+              <div className="info">{item.shortText}</div>
             </div>
           );
         })}
